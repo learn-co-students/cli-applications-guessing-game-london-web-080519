de1b97a80115f235f4eb88e3e7ef7e94a8817648
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 19:19:56 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+
+def run_guessing_game
+  random_number = rand(1::6)
+
+  puts "Guess the number"
+
+  guess = gets.strip
+
+  case guess
+  when random_number
+    puts "You guessed the correct cumber!"
+  when "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{random_number}."
+  end
+end
